60a52fd1b934dc3664b37e31d793eacb66caff61
Mia Bueno <mia.bueno101@gmail.com>
2021-01-29 15:15:15 +1100

    init a pattern

---
+++ game.js
@@ -0,0 +1,9 @@
+
+buttonColours = ["red", "blue", "green", "yellow"];
+gamePattern = [];
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+}
